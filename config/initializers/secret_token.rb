a92e86583892da9cdee94823329a10990cc6c866
Channing <cchanningallen@gmail.com>
2013-08-31 10:06:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crossfit::Application.config.secret_key_base = 'c2d3ec1bc568b2080524480204bc735750e03dd720ac9c928b7ea4912844fdd1eda07d8c661186b35e096bea0c2a57995feb5cb3576a6e1f251d0ad8ac37a352'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crossfit::Application.config.session_store :cookie_store, key: '_crossfit_session'
